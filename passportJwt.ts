782c37d4ff69c3658b95be3464bbb33b3e4177e0
tuan.pham.21.01.2003 <tuan.pham.21.01.2003@gmail.com>
2024-06-27 13:47:12 +0000

    fix

--- passportJwt.ts
+++ passportJwt.ts
@@ -1,29 +1,25 @@
 import User from "./mongoose_schemas/user"
 
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+import passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 const options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_SECRET_KEY
 };
 
-passport.use(new JwtStrategy(options, async(jwtPayload, done) => {
-    try
-    {
+passport.use(new JwtStrategy(options, async (jwtPayload, done) => {
+    try {
         const user = await User.findById(jwtPayload.sub).exec();
-        if (user)
-        {
+        if (user) {
             return done(null, user);
         }
-        else
-        {
+        else {
             return done(null, false);
         }
     }
-    catch(err)
-    {
+    catch (err) {
         return done(err, false);
     }
 }));
