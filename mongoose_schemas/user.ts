654c5ad2652397db765a865c7ec5c12701a32a74
ZeVnan <thienan27103@gmail.com>
2024-07-02 03:07:46 +0700

    generate user

--- mongoose_schemas/user.ts
+++ mongoose_schemas/user.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import * as bcrypt from "bcryptjs";
+import { NextFunction } from "express";
 
 export enum UserRole {
     Sender = "sender",
@@ -11,14 +12,15 @@ export enum UserRole {
 export interface IUser extends mongoose.Document {
     username: string;
     password: string;
-    email: string;
+    id: number;
     role: UserRole;
     ComparePassword(password: string): Promise<boolean>;
 }
 
 const user: mongoose.Schema<IUser> = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     password: { type: String, required: true },
+    id: { type: Number, required: true, unique: true },
     role: { type: String, enum: UserRole, required: true },
 });
 
@@ -32,6 +34,28 @@ user.pre<IUser>("save", async function (next) {
 user.methods.ComparePassword = function (password: string): boolean {
     return password == this.password;
 };
+
+export async function EnsureUserExists(req, res, next) {
+    const roles = Object.values(UserRole);
+    const promises = roles.map(async (role) => {
+        for (let i = 0; i < 10; i++){
+            const newUsername = `${role}_username_${i}`;
+            const newPassword = `${role}_password_${i}`;
+            const isUserExist = await User.findOne({newUsername}).exec();
+            if (!isUserExist){
+                const newUser = new User({
+                    username: newUsername,
+                    password: newPassword,
+                    id: i,
+                    role: role
+                })
+                await newUser.save();
+            }
+        }
+    });
+    await Promise.all(promises);
+    next();
+}
 
 const User = mongoose.model("User", user);
 
