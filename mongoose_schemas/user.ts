afa41854b94a2212663d65879de48eb9deeb96f8
tuan.pham.21.01.2003 <tuan.pham.21.01.2003@gmail.com>
2024-07-02 00:49:32 +0000

    fix

--- mongoose_schemas/user.ts
+++ mongoose_schemas/user.ts
@@ -1,55 +1,51 @@
-import mongoose from "mongoose";
-import * as bcrypt from "bcryptjs";
-import { NextFunction } from "express";
+import mongoose from "mongoose"; import * as express from "express"; import * as bcrypt from "bcryptjs";
 
 export enum UserRole {
-    Sender = "sender",
+      Sender =   "sender",
     Receiver = "receiver",
-    Shipper = "shipper",
-    Coordinator = "coordinator"
+    Shipper  = "shipper",
+    Coordinator = "coordinator",
 }
 
 export interface IUser extends mongoose.Document {
     username: string;
     password: string;
-    id: number;
     role: UserRole;
     ComparePassword(password: string): Promise<boolean>;
 }
 
 const user: mongoose.Schema<IUser> = new mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    id: { type: Number, required: true, unique: true },
-    role: { type: String, enum: UserRole, required: true },
+    username: { type: String, required: true, index: true, unique: true },
+    password: { type: String, required: true,                           },
+    role: { type: String, enum: UserRole,  required: true               },
 });
 
 user.pre<IUser>("save", async function (next) {
-    if (this.isModified("password") || this.isNew) {
+    if (this.isModified("password")
+    ||  this.isNew) {
         this.password = await bcrypt.hash(this.password, 10);
     }
     next();
 });
 
-user.methods.ComparePassword = function (password: string): boolean {
-    return password == this.password;
+user.methods.ComparePassword =function (password: string): Promise<boolean> {
+    return bcrypt.compare(password,this.password);
 };
 
-export async function EnsureUserExists(req, res, next) {
+export const ensureUserExists: express.RequestHandler = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
     const roles = Object.values(UserRole);
-    const promises = roles.map(async (role) => {
-        for (let i = 0; i < 10; i++){
-            const newUsername = `${role}_username_${i}`;
-            const newPassword = `${role}_password_${i}`;
-            const isUserExist = await User.findOne({newUsername}).exec();
-            if (!isUserExist){
+    const promises = (Array.from({length: 10}, (_, index) => index + 1)).map(async (index) => {
+        for (var role of roles) {
+            const newUsername = `${role}_username_${index}`;
+            const newPassword = `${role}_password_${index}`;
+            if (!(await User.exists({ username: newUsername }))) {
                 const newUser = new User({
                     username: newUsername,
                     password: newPassword,
-                    id: i,
                     role: role
                 })
                 await newUser.save();
+                console.log("YES");
             }
         }
     });
@@ -60,3 +56,8 @@ export async function EnsureUserExists(req, res, next) {
 const User = mongoose.model("User", user);
 
 export default User;
+
+export
+type          TUser = mongoose.Document<unknown, {}, IUser> & IUser & Required<{
+    _id: unknown;
+}>;
