62263f6ca8341bbfc5f8d20d3538bd2e27e56b07
bcluc <21521898@gm.uit.edu.vn>
2024-07-04 03:47:04 +0700

    assign

--- pythonClient/flask-server/arrange.py
+++ pythonClient/flask-server/arrange.py
@@ -2,13 +2,18 @@ from flask import Flask, request, jsonify, Blueprint # type: ignore
 from pymongo import MongoClient # type: ignore
 from bson.objectid import ObjectId # type: ignore
 from bson import json_util # type: ignore
+from datetime import datetime
+from ortools.constraint_solver import routing_enums_pb2 # type: ignore
+from ortools.constraint_solver import pywrapcp # type: ignore
 import json
+import requests  # type: ignore
 import os
 
 arrange_bp = Blueprint("arrange",__name__)
 
 # Connect to MongoDB
 client = MongoClient(os.environ.get("MONGO_DB"))
+maps_key = os.environ.get("GOOGLE_MAP_API")
 db = client.lift
 
 # Collections
@@ -24,13 +29,13 @@ sample_hubs = [
 ]
 
 sample_orders = [
-    {"_id": ObjectId("66851e166a086a68a695c187"), "value": 1200000, "weight": 50, "hub_id": ObjectId("66851d686a086a68a695c186"), "status": "pending", "delivery_address": "123 Vo Thi Sau, District 1, HCM City"},
-    {"_id": ObjectId("66851e166a086a68a695c188"), "value": 800000, "weight": 30, "hub_id": ObjectId("66851d686a086a68a695c187"), "status": "pending", "delivery_address": "456 Nguyen Trai, District 2, HCM City"}
+    {"_id": ObjectId("66851e166a086a68a695c187"), "value": 1200000, "weight": 50, "hubId": ObjectId("66851d686a086a68a695c186"), "status": "pending", "deliveryAddress": "123 Vo Thi Sau, District 1, HCM City"},
+    {"_id": ObjectId("66851e166a086a68a695c188"), "value": 800000, "weight": 30, "hubId": ObjectId("66851d686a086a68a695c187"), "status": "pending", "deliveryAddress": "456 Nguyen Trai, District 2, HCM City"}
 ]
 
 sample_staff = [
-    {"_id": ObjectId("66851d316a086a68a695c185"), "name": "John Doe", "age": 25, "gender": "male", "weight": 70, "motorcycle_capacity": 150, "hub_id": ObjectId("66851d686a086a68a695c186")},
-    {"_id": ObjectId("66851d316a086a68a695c186"), "name": "Jane Smith", "age": 30, "gender": "female", "weight": 60, "motorcycle_capacity": 100, "hub_id": ObjectId("66851d686a086a68a695c187")}
+    {"_id": ObjectId("66851d316a086a68a695c185"), "name": "John Doe", "age": 25, "gender": "male", "weight": 70, "motorcycleCapacity": 150, "hubId": ObjectId("66851d686a086a68a695c186")},
+    {"_id": ObjectId("66851d316a086a68a695c186"), "name": "Jane Smith", "age": 30, "gender": "female", "weight": 60, "motorcycleCapacity": 100, "hubId": ObjectId("66851d686a086a68a695c187")}
 ]
 
 # Insert sample data into the collections
@@ -53,20 +58,92 @@ def parse_json(data):
     data = json.loads(json_util.dumps(data))
     if isinstance(data, list):
         for item in data:
-            if 'hub_id' in item:
-                item['hub_id'] = str(item['hub_id']["$oid"])
+            if 'hubId' in item:
+                item['hubId'] = str(item['hubId']["$oid"])
             if '_id' in item:
                 item['_id'] = str(item['_id']["$oid"])
-            if 'inserted_id' in item:
-                item['inserted_id'] = str(item['inserted_id']["$oid"])
+            if 'insertedId' in item:
+                item['insertedId'] = str(item['insertedId']["$oid"])
     else:
-        if 'hub_id' in data:
-            data['hub_id'] = str(data['hub_id']["$oid"])
+        if 'hubId' in data:
+            data['hubId'] = str(data['hubId']["$oid"])
         if '_id' in data:
                 data['_id'] = str(data['_id']["$oid"])
-        if 'inserted_id' in data:
-                data['inserted_id'] = str(data['inserted_id']["$oid"])
+        if 'insertedId' in data:
+                data['insertedId'] = str(data['insertedId']["$oid"])
     return data
+
+def geocode_address(address):
+    # url = f'https://geocode.maps.co/search?q='+address+'&api_key='+maps_key
+    response = requests.get(url)
+    if response.status_code == 200:
+        result = parse_json(response)
+        if result:
+            location = result[0]
+            print("hi")
+            return parse_json({"lat": location['lat'], "lon" : location['lon']})
+    return parse_json({"response": 'none'})
+
+@arrange_bp.route('/checkgeocode', methods = ["GET"])
+def check():
+    response= geocode_address("1600 Amphitheatre Parkway, Mountain View, CA")
+    return parse_json(response)
+
+# Helper function to calculate distance between two locations (latitude, longitude)
+def calculate_distance(lat1, lon1, lat2, lon2):
+    # This function calculates the Haversine distance between two points on the Earth
+    from math import radians, cos, sin, asin, sqrt
+    R = 6371  # Radius of the earth in km
+    dlat = radians(lat2 - lat1)
+    dlon = radians(lon2 - lon1)
+    a = sin(dlat / 2) ** 2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    distance = R * c
+    return distance
+
+# Use OR-Tools to calculate the optimal routes
+def create_data_model(orders, staff, hub):
+    data = {}
+    hub_lat, hub_lng = geocode_address(hub['address'])
+    data['distance_matrix'] = []
+    all_locations = [(hub_lat, hub_lng)] + [geocode_address(order['delivery_address']) for order in orders]
+    for loc1 in all_locations:
+        distances = []
+        for loc2 in all_locations:
+            distances.append(calculate_distance(loc1['lat'], loc1['lng'], loc2['lat'], loc2['lng']))
+        data['distance_matrix'].append(distances)
+    data['numVehicles'] = len(staff)
+    data['depot'] = 0
+    return data
+
+def solve_vrp(data):
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']), data['num_vehicles'], data['depot'])
+    routing = pywrapcp.RoutingModel(manager)
+
+    def distance_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    solution = routing.SolveWithParameters(search_parameters)
+    if not solution:
+        return None
+
+    routes = []
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        route = []
+        while not routing.IsEnd(index):
+            route.append(manager.IndexToNode(index))
+            index = solution.Value(routing.NextVar(index))
+        routes.append(route)
+    return routes
 
 # Hubs Endpoints
 
@@ -77,28 +154,28 @@ def get_all_hubs():
 
 @arrange_bp.route('/hub', methods=['GET'])
 def get_hub():
-    hub_id = request.args.get('id')
-    hub = hubs.find_one({'_id': ObjectId(hub_id)})
+    hubId = request.args.get('id')
+    hub = hubs.find_one({'_id': ObjectId(hubId)})
     return parse_json(hub), 200
 
 @arrange_bp.route('/hub', methods=['POST'])
 def create_hub():
     data = request.json
     result = hubs.insert_one(data)
-    return parse_json({'inserted_id': result.inserted_id}), 201
+    return parse_json({'insertedId': result.insertedId}), 201
 
 @arrange_bp.route('/hub', methods=['PUT'])
 def update_hub():
-    hub_id = request.args.get('id')
+    hubId = request.args.get('id')
     data = request.json
-    result = hubs.update_one({'_id': ObjectId(hub_id)}, {'$set': data})
-    return parse_json({'matched_count': result.matched_count, 'modified_count': result.modified_count}), 200
+    result = hubs.update_one({'_id': ObjectId(hubId)}, {'$set': data})
+    return parse_json({'matchedCount': result.matchedCount, 'modifiedCount': result.modifiedCount}), 200
 
 @arrange_bp.route('/hub', methods=['DELETE'])
 def delete_hub():
-    hub_id = request.args.get('id')
-    result = hubs.delete_one({'_id': ObjectId(hub_id)})
-    return parse_json({'deleted_count': result.deleted_count}), 200
+    hubId = request.args.get('id')
+    result = hubs.delete_one({'_id': ObjectId(hubId)})
+    return parse_json({'deletedCount': result.deletedCount}), 200
 
 # Orders Endpoints
 
@@ -109,29 +186,29 @@ def get_all_orders():
 
 @arrange_bp.route('/order', methods=['GET'])
 def get_order():
-    order_id = request.args.get('id')
-    order = orders.find_one({'_id': ObjectId(order_id)})
+    orderId = request.args.get('id')
+    order = orders.find_one({'_id': ObjectId(orderId)})
     return parse_json(order), 200
 
 @arrange_bp.route('/order', methods=['POST'])
 def create_order():
     data = request.json
-    data['hub_id'] = ObjectId(data['hub_id'])
+    data['hubId'] = ObjectId(data['hubId'])
     result = orders.insert_one(data)
-    return parse_json({'inserted_id': result.inserted_id}), 201
+    return parse_json({'insertedId': result.insertedId}), 201
 
 @arrange_bp.route('/order', methods=['PUT'])
 def update_order():
-    order_id = request.args.get('id')
+    orderId = request.args.get('id')
     data = request.json
-    result = orders.update_one({'_id': ObjectId(order_id)}, {'$set': data})
-    return parse_json({'matched_count': result.matched_count, 'modified_count': result.modified_count}), 200
+    result = orders.update_one({'_id': ObjectId(orderId)}, {'$set': data})
+    return parse_json({'matchedCount': result.matchedCount, 'modifiedCount': result.modifiedCount}), 200
 
 @arrange_bp.route('/order', methods=['DELETE'])
 def delete_order():
-    order_id = request.args.get('id')
-    result = orders.delete_one({'_id': ObjectId(order_id)})
-    return parse_json({'deleted_count': result.deleted_count}), 200
+    orderId = request.args.get('id')
+    result = orders.delete_one({'_id': ObjectId(orderId)})
+    return parse_json({'deletedCount': result.deletedCount}), 200
 
 # Staff Endpoints
 
@@ -142,56 +219,85 @@ def get_all_staff():
 
 @arrange_bp.route('/staff', methods=['GET'])
 def get_staff():
-    staff_id = request.args.get('id')
-    staff_member = staff.find_one({'_id': ObjectId(staff_id)})
+    staffId = request.args.get('id')
+    staff_member = staff.find_one({'_id': ObjectId(staffId)})
     return parse_json(staff_member), 200
 
 @arrange_bp.route('/staff', methods=['POST'])
 def create_staff():
     data = request.json
-    data['hub_id'] = ObjectId(data['hub_id'])
+    data['hubId'] = ObjectId(data['hubId'])
     result = staff.insert_one(data)
-    return parse_json({'inserted_id': result.inserted_id}), 201
+    return parse_json({'insertedId': result.insertedId}), 201
 
 @arrange_bp.route('/staff', methods=['PUT'])
 def update_staff():
-    staff_id = request.args.get('id')
+    staffId = request.args.get('id')
     data = request.json
-    result = staff.update_one({'_id': ObjectId(staff_id)}, {'$set': data})
-    return parse_json({'matched_count': result.matched_count, 'modified_count': result.modified_count}), 200
+    result = staff.update_one({'_id': ObjectId(staffId)}, {'$set': data})
+    return parse_json({'matchedCount': result.matchedCount, 'modifiedCount': result.modifiedCount}), 200
 
 @arrange_bp.route('/staff', methods=['DELETE'])
 def delete_staff():
-    staff_id = request.args.get('id')
-    result = staff.delete_one({'_id': ObjectId(staff_id)})
-    return parse_json({'deleted_count': result.deleted_count}), 200
+    staffId = request.args.get('id')
+    result = staff.delete_one({'_id': ObjectId(staffId)})
+    return parse_json({'deletedCount': result.deletedCount}), 200
 
-# Assign Delivery Task Endpoint
+# Assign endpoints
 
 @arrange_bp.route('/assign', methods=['POST'])
-def assign_delivery_task():
-    hub_id = request.json['hub_id']
-    orders_pending = orders.find({'hub_id': ObjectId(hub_id), 'status': 'pending'})
-    staff_members = staff.find({'hub_id': ObjectId(hub_id)})
+def assign_delivery_tasks():
+    hub_id = request.json['hubId']
+    hub = hubs.find_one({'_id': ObjectId(hub_id)})
+    orders_pending = list(orders.find({'hubId': ObjectId(hub_id), 'status': 'pending'}))
+    staff_members = list(staff.find({'hubId': ObjectId(hub_id)}))
+
+    if not hub or not orders_pending or not staff_members:
+        return jsonify({'error': 'Invalid data'}), 400
+
+    data = create_data_model(orders_pending, staff_members, hub)
+    routes = solve_vrp(data)
+
+    if not routes:
+        return jsonify({'error': 'No solution found'}), 400
 
     assignments = []
+    for vehicle_id, route in enumerate(routes):
+        staff_member = staff_members[vehicle_id]
+        for idx in route[1:]:
+            order = orders_pending[idx - 1]
+            assignment = {
+                'staffId': str(staff_member['_id']),
+                'orderId': str(order['_id']),
+                'date': datetime.utcnow(),
+                'deliverTimes': 0,
+                'status': 'pending'
+            }
+            delivery.insert_one(assignment)
+            assignments.append(assignment)
 
-    for order in orders_pending:
-        for member in staff_members:
-            if (order['weight'] <= member['motorcycle_capacity'] and
-                    ((member['age'] <= 30 and order['value'] > 1000000) or
-                    (member['age'] > 30 and order['value'] <= 1000000))):
-                assignments.append({
-                    'order_id': order['_id'],
-                    'staff_id': member['_id'],
-                    'delivery_id': order['hub_id']
-                })
-                orders.update_one({'_id': order['_id']}, {'$set': {'status': 'assigned'}})
-                break
+            orders.update_one({'_id': order['_id']}, {'$set': {'status': 'delivering'}})
 
-    return parse_json(assignments), 200
+    return jsonify(parse_json(assignments)), 200
 
+@arrange_bp.route('/transfer/update_status', methods=['POST'])
+def update_transfer_status():
+    transfer_id = request.json['transferId']
+    status = request.json['status']
+    transfer = delivery.find_one({'_id': ObjectId(transfer_id)})
 
+    if not transfer:
+        return jsonify({'error': 'Transfer not found'}), 400
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+    if status == 'success':
+        delivery.update_one({'_id': ObjectId(transfer_id)}, {'$set': {'status': 'success'}})
+        orders.update_one({'_id': ObjectId(transfer['orderId'])}, {'$set': {'status': 'delivered'}})
+    elif status == 'failed':
+        deliver_times = transfer.get('deliverTimes', 0) + 1
+        if deliver_times >= 3:
+            delivery.update_one({'_id': ObjectId(transfer_id)}, {'$set': {'status': 'failed'}})
+            orders.update_one({'_id': ObjectId(transfer['orderId'])}, {'$set': {'status': 'failed'}})
+        else:
+            delivery.update_one({'_id': ObjectId(transfer_id)}, {'$set': {'deliverTimes': deliver_times}})
+
+    return jsonify({'status': 'updated'}), 200
