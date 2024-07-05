93215d6b674e9282f7b8a9ddab93b3f9ff3158f0
tuan.pham.21.01.2003 <tuan.pham.21.01.2003@gmail.com>
2024-07-05 23:49:38 +0000

    fix assign

--- router/delivery.ts
+++ router/delivery.ts
@@ -5,8 +5,12 @@ import mongoose from "mongoose";
 const router = express.Router();
 
 router.post("/delivery/update_status", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const response = await axios.post("http://pythonserver:27018/delivery/update_status", req.body);
-    res.json(response.data);
+    try {
+        const response = await axios.post("http://pythonserver:27018/delivery/update_status", req.body);
+        res.json(response.data);
+    } catch (err) {
+        next(err);
+    }
 });
 
 router.get("/delivery/id", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
