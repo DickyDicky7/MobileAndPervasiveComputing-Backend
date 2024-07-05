a278a3ee58770ffc87c5e03598653384a041f58c
tuan.pham.21.01.2003 <tuan.pham.21.01.2003@gmail.com>
2024-07-05 21:37:34 +0000

    fixx up img

--- router/delivery.ts
+++ router/delivery.ts
@@ -18,4 +18,31 @@ router.get("/delivery/id", async (req: express.Request, res: express.Response, n
     }
 });
 
+router.get("/deliveries", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const response = await axios.get(`http://pythonserver:27018/deliveries`);
+        res.json(response.data);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get("/deliveries/hub", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const response = await axios.get(`http://pythonserver:27018/deliveries/hub?hubId=${req.query.hubId}`);
+        res.json(response.data);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get("/deliveries/staff", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const response = await axios.get(`http://pythonserver:27018/deliveries/staff?staffId=${req.query.staffId}`);
+        res.json(response.data);
+    } catch (err) {
+        next(err);
+    }
+});
+
 export default router;
