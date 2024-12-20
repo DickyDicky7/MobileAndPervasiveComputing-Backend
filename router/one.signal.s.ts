7e2d9f2fa0017fb8cb0b458f6c392f0d7c264fdc
tuan.pham.21.01.2003 <tuan.pham.21.01.2003@gmail.com>
2024-12-20 11:32:02 +0000

    fix

--- router/one.signal.s.ts
+++ router/one.signal.s.ts
@@ -12,13 +12,21 @@ import mongoose from
 
 
             
-export const OneSignalSendPushNotificationTo = async (userId: string | mongoose.Types.ObjectId, contentEn: string): Promise<AxiosResponse<any, any>> => {
+export const OneSignalSendPushNotificationTo = async (userId: string | mongoose.Types.ObjectId, contentEn: string, headingEn: string, subtitleEn: string): Promise<AxiosResponse<any, any>> => {
     const result = await axios.post("https://api.onesignal.com/notifications/"
     ,
     {
         app_id: "9aea25d1-efcd-4578-9fad-ffeb401ae64e"
     ,   contents: {
             en: contentEn
+         ,
+        },
+        headings: {
+            en: headingEn
+         ,
+        },
+        subtitle: {
+            en: subtitleEn
          ,
         },
          target_channel: "push",
@@ -47,16 +55,16 @@ export const OneSignalSendPushNotificationTo = async (userId: string | mongoose.
 
 router.post("/one-signal/send-push-notification", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const { userId ,  contentEn } = req.body;
-        if    ( userId && contentEn ) {
+        const { userId,   contentEn,   headingEn,   subtitleEn } = req.body;
+        if    ( userId && contentEn && headingEn && subtitleEn ) {
 //          ----------------------------------------------------
             const result = await OneSignalSendPushNotificationTo
 //          ----------------------------------------------------
-              ( userId ,  contentEn )  ;
+              ( userId,   contentEn,   headingEn,   subtitleEn ) ;
             res.status(200).json(result.data);
 //          res.status(200).json(result     );
         } else {
-            res.status(400).json({ "msg": "Invalid request | reason: userId or contentEn is misssing" });
+            res.status(400).json({ "msg": "Invalid request | reason: userId or contentEn or headingEn or subtitleEn is misssing" });
         }
     } catch (err) {
         next(err) ;
