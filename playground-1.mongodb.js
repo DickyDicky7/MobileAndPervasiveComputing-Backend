1b6dc3109b289f73193ef7b589befaa6c95da43c
tuan.pham.21.01.2003 <tuan.pham.21.01.2003@gmail.com>
2024-12-25 09:30:26 +0000

    mongodb playground

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,18 +1,18 @@
-let shippers = []
+let shippers = [];
 db.users.find().forEach(user => {
-    if (user.role === "shipper") {
-        shippers.push(user)
-    }
-})
+                    if (user.role === "shipper") {
+    shippers.push(user);
+                                                 };
+});
 
 function splitArrayIntoGroups(array, n) {
     if (n <= 0) {
         throw new Error("Group size must be greater than zero.");
     }
 
-    const result = [];
-    for (let i = 0; i < array.length; i += n) {
-        result.push(array.slice(i, i + n));
+    const  result = [];
+    for (let i = 0; i < array.length;  i += n) {
+           result.push( array.slice(i, i +  n));
     }
     return result;
 }
@@ -28,17 +28,17 @@ function getRandomBool() {
     return Math.random() < 0.5;
 }
 
-groups.forEach((group, groupIndex) => {
-    group.forEach((shipper, shipperIndex) => {
+groups.forEach((  group,   groupIndex) => {
+group .forEach((shipper, shipperIndex) => {
         let hub = db.hubs.findOne({ name: `Hub ${groupIndex + 1}` })
         db.staffs.insertOne({
-            name: `SHIPPER ${shipperIndex + 1} HUB ${groupIndex + 1}`,
-            age: getRandomInt(18, 50),
-            gender: getRandomBool() ? "male" : "female",
-            weight: getRandomInt(50, 100),
-            motorcycleCapacity: getRandomInt(100, 400),
-            hubId: hub._id,
-            userId: shipper._id,
+            name              : `SHIPPER ${shipperIndex + 1} HUB ${groupIndex + 1}`,
+            age               : getRandomInt( 18,  50)             ,
+            gender            : getRandomBool() ? "male" : "female",
+            weight            : getRandomInt( 50, 100)             ,
+            motorcycleCapacity: getRandomInt(100, 400)             ,
+             hubId            :     hub._id,
+            userId            : shipper._id,
         })
     })
 })
